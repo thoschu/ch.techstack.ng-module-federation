df4c57e441d5b0b003f806c146eb4bc3464d994f
Thomas-L. Schulte <tls@artegic.de>
2023-03-07 15:17:40 +0100

    update

---
+++ apps/cart/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-module-federation-cart-entry',
+  template: `<ng-module-federation-nx-welcome></ng-module-federation-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
